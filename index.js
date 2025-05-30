b7f905d7c53f0da9929cd26df28a80cd758d3c1b
biito24 <156735756+biito24@users.noreply.github.com>
2025-05-30 10:01:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const correcAnswers = ['B','B','B','B']
+const correcAnswers = ['B','B','B','B', 'A']
 const form = document.querySelector('.quiz-form')
 const result = document.querySelector('.result')
 
 form.addEventListener('submit' , e =>{
   e.preventDefault()
   let score = 0
-  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value]
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value]
   // check answer
   userAnswers.forEach( (answer, index) =>{
     if(answer === correcAnswers[index]){
